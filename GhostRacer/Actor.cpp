2671688e976f2fefcee6b2735e3a51002732ce94
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-02-20 03:06:55 -0800

    grahpics

--- GhostRacer/Actor.cpp
+++ GhostRacer/Actor.cpp
@@ -2,3 +2,34 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+actor::actor() 
+{
+
+}
+
+ghostRacer::ghostRacer()
+{
+
+}
+
+ghostRacer::~ghostRacer()
+{
+	delete car;
+}
+
+void ghostRacer::doSomething()
+{
+
+}
+
+borderLines::borderLines()
+{
+
+}
+
+borderLines::~borderLines()
+{
+	delete whiteLine;
+	delete yellowLine;
+}
--- GhostRacer/Actor.h
+++ GhostRacer/Actor.h
@@ -5,4 +5,79 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class actor :public GraphObject
+{
+public:
+	actor();
+
+private:
+};
+
+class ghostRacer :public actor
+{
+public:
+	ghostRacer();
+	~ghostRacer();
+	void doSomething();
+private:
+	GraphObject* car;
+};
+
+class borderLines :public actor
+{
+public:
+	borderLines();
+	~borderLines();
+private:
+	GraphObject* whiteLine;
+	GraphObject* yellowLine;
+};
+
+/*
+class goodies :public actor
+{
+
+};
+
+class holyWater :public goodies
+{
+
+};
+
+class health :public goodies
+{
+
+};
+
+class humanPedestrian :public actor
+{
+
+};
+
+class zombiePedestrian :public actor
+{
+
+};
+
+class taxi :public actor
+{
+
+};
+
+class souls :public actor
+{
+
+};
+
+class cannon :public actor
+{
+
+};
+
+class oilSlicks :public actor
+{
+
+};
+*/
+
 #endif // ACTOR_H_
