b1628b11f569968eecc2270e4463e7ea53acb4ed
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-02-20 11:05:19 -0800

    backup

--- GhostRacer/Actor.h
+++ GhostRacer/Actor.h
@@ -5,4 +5,112 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class StudentWorld;
+
+class actor :public GraphObject
+{
+public:
+	actor(int m_ID, double m_X, double m_Y, int m_dir, double m_size, int m_depth, 
+		bool alive, bool collision, int vspeed, int hspeed, StudentWorld* world);
+	bool getLivingStatus();
+	void setLivingStatus(bool dedOrAlive);
+	StudentWorld* getWorld();
+	virtual void doSomething();
+	virtual void movement();
+	int getVSpeed();
+	void setVSpeed(int v_s);
+	int getHSpeed();
+	void setHSpeed(int h_s);
+
+private:
+	bool if_alive;
+	bool has_collision;
+	int vert_speed;
+	int horiz_speed;
+	StudentWorld* m_world;
+};
+
+class ghostRacer :public actor
+{
+public:
+	ghostRacer(StudentWorld* world);
+	virtual void doSomething();
+	virtual void movement();
+	
+private:
+	int m_health;
+	int m_spray;
+};
+
+//new
+class whiteLines :public actor
+{
+public:
+	whiteLines(double line_X, double line_Y, StudentWorld* world, ghostRacer* racer);
+	void doSomething();
+private:
+	ghostRacer* car;
+};
+
+class yellowLines :public actor
+{
+public:
+	yellowLines(double line_X, double line_Y, StudentWorld* world, ghostRacer* racer);
+	void doSomething();
+private:
+	ghostRacer* car;
+};
+//new
+
+/*class borderLines :public actor
+{
+public:
+	borderLines(int line_ID, double line_X, double line_Y, StudentWorld* world);
+private:
+	
+};
+
+class whiteLines :borderLines
+{
+public:
+	whiteLines(double line_X, double line_Y, StudentWorld* world);
+private:
+};
+
+class yellowLines :borderLines
+{
+public:
+	yellowLines(double line_X, double line_Y, StudentWorld* world);
+private:
+};
+
+class goodies :public actor
+{
+};
+class holyWater :public goodies
+{
+};
+class health :public goodies
+{
+};
+class humanPedestrian :public actor
+{
+};
+class zombiePedestrian :public actor
+{
+};
+class taxi :public actor
+{
+};
+class souls :public actor
+{
+};
+class cannon :public actor
+{
+};
+class oilSlicks :public actor
+{
+};
+*/
+
 #endif // ACTOR_H_
--- GhostRacer/StudentWorld.cpp
+++ GhostRacer/StudentWorld.cpp
@@ -1,33 +1,75 @@
 #include "StudentWorld.h"
 #include "GameConstants.h"
+#include <iostream>
+#include <sstream>
 #include <string>
 using namespace std;
 
 GameWorld* createStudentWorld(string assetPath)
 {
-	return new StudentWorld(assetPath);
+    return new StudentWorld(assetPath);
 }
 
 // Students:  Add code to this file, StudentWorld.h, Actor.h, and Actor.cpp
 
 StudentWorld::StudentWorld(string assetPath)
-: GameWorld(assetPath)
+    : GameWorld(assetPath)
 {
+    racer = nullptr;
+}
+
+StudentWorld::~StudentWorld()
+{
+    cleanUp();
 }
 
 int StudentWorld::init()
 {
+    //setGameStatText(text());
+    racer = new ghostRacer(this);
+    int N = VIEW_HEIGHT / SPRITE_HEIGHT;
+    int M = VIEW_HEIGHT / (4 * SPRITE_HEIGHT);
+    int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH / 2;
+    int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH / 2;
+    for (double j = 0; j < N; j++)
+    {
+        actors.push_back(new yellowLines(LEFT_EDGE, j * SPRITE_HEIGHT, this, racer));
+        actors.push_back(new yellowLines(RIGHT_EDGE, j * SPRITE_HEIGHT, this, racer));
+    }
+    for (double j = 0; j < M; j++)
+    {
+        actors.push_back(new whiteLines(LEFT_EDGE + ROAD_WIDTH / 3.0, j * (4.0 * SPRITE_HEIGHT), this, racer));
+        actors.push_back(new whiteLines(RIGHT_EDGE - ROAD_WIDTH / 3.0, j * (4.0 * SPRITE_HEIGHT), this, racer));
+    }
+
     return GWSTATUS_CONTINUE_GAME;
 }
 
 int StudentWorld::move()
 {
-    // This code is here merely to allow the game to build, run, and terminate after you hit enter.
-    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-    decLives();
-    return GWSTATUS_PLAYER_DIED;
+    //setGameStatText(text());
+    racer->doSomething();
+    vector <actor*> ::iterator it;
+    for (it = actors.begin(); it != actors.end(); it++)
+        (*it)->doSomething();
+    return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp()
 {
+    delete racer;
+    vector <actor*> ::iterator it;
+    for (it = actors.begin(); it != actors.end(); it++)
+        delete *it;
 }
+
+/*
+string StudentWorld::text()
+{
+    ostringstream top_text;
+    top_text << "Score: " << getScore() << "  Lvl: " << getLevel() << "  Souls2Save: " << "5"
+        << "  Lives: " << getLives() << "  Health: " << "100" << "  Sprays: " << "10" << "  Bonus: " << "5000";
+    string s = top_text.str();
+    return s;
+}
+*/
