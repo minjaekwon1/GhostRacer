f98f7adcea7ff69450b9cb5e10a80651342e531f
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-02-27 05:56:50 -0800

    Update StudentWorld.h

--- GhostRacer/StudentWorld.h
+++ GhostRacer/StudentWorld.h
@@ -24,11 +24,23 @@ public:
     //determines if actor will be generated
     int spawnRate(int a, int b);
 
+    // Record that a soul was saved.
+    void recordSoulSaved();
+
+    // If actor a overlaps some live actor that is affected by a holy water
+    // projectile, inflict a holy water spray on that actor and return true;
+    // otherwise, return false.  (See Actor::beSprayedIfAppropriate.)
+    bool sprayFirstAppropriateActor(actor* a);
+
+    // Return true if actor a1 overlaps actor a2, otherwise false.
+    bool overlaps(const actor* a1, const actor* a2) const;
+
 private:
     ghostRacer* racer;
     std::vector <actor*> actors;
     int lineTracker;
     int level;
+    int numOfSoulsSaved;
 };
 
 #endif // STUDENTWORLD_H_
