93e32d694965deef744e2522daed0726ca09f17f
ProjectFAR <71264291+ProjectFAR@users.noreply.github.com>
2021-02-27 18:38:26 -0800

    Update StudentWorld.cpp

--- GhostRacer/StudentWorld.cpp
+++ GhostRacer/StudentWorld.cpp
@@ -18,8 +18,8 @@ StudentWorld::StudentWorld(string assetPath)
 {
     racer = nullptr;
     lineTracker = -1;
-    level = 1;
-    numOfSoulsSaved = 0;
+    numOfSoulsNeeded = 0;
+    bonusPoints = 0;
 }
 
 StudentWorld::~StudentWorld()
@@ -34,7 +34,9 @@ ghostRacer* StudentWorld::getGhostRacer()
 
 int StudentWorld::init()
 {
-    //setGameStatText(text());
+    numOfSoulsNeeded = 2 * getLevel() + 5;
+    bonusPoints = 5000;
+
     racer = new ghostRacer(this);
     int N = VIEW_HEIGHT / SPRITE_HEIGHT;
     int M = VIEW_HEIGHT / (4 * SPRITE_HEIGHT);
@@ -51,17 +53,27 @@ int StudentWorld::init()
         actors.push_back(new whiteLines(RIGHT_EDGE - ROAD_WIDTH / 3.0, j * (4.0 * SPRITE_HEIGHT), this));
     }
     lineTracker = actors.back()->getY();
-
+    
+    setGameStatText(text());
     return GWSTATUS_CONTINUE_GAME;
 }
 
 int StudentWorld::move()
 {
-    //setGameStatText(text());
+    int L = getLevel();
     racer->doSomething();
     for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); it++)
     {
         (*it)->doSomething();
+        if (racer->getHP() <= 0)
+        {
+            decLives();
+            return GWSTATUS_PLAYER_DIED;
+        }
+        if (numOfSoulsNeeded <= 0)
+        {
+            return GWSTATUS_FINISHED_LEVEL;
+        }
     }
 
     for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); )
@@ -80,7 +92,9 @@ int StudentWorld::move()
     int new_border_y = VIEW_HEIGHT - SPRITE_HEIGHT;
     int delta_y = new_border_y - lineTracker;
     int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH / 2;
+    int LEFT_INNER_EDGE = ROAD_CENTER - ROAD_WIDTH / 6;
     int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH / 2;
+    int RIGHT_INNER_EDGE = ROAD_CENTER + ROAD_WIDTH / 6;
     
     if (delta_y >= SPRITE_HEIGHT)
     {
@@ -103,7 +117,7 @@ int StudentWorld::move()
     if (spawnRate(150, 40) == 0)
         actors.push_back(new OilSlick(randInt(LEFT_EDGE, RIGHT_EDGE), VIEW_HEIGHT, this));
 
-    int ChanceOfHolyWater = 100 + 10 * level;
+    int ChanceOfHolyWater = 100 + 10 * L;
     if (randInt(0, ChanceOfHolyWater - 1) == 0)
         actors.push_back(new HolyWaterGoodie(randInt(LEFT_EDGE, RIGHT_EDGE), VIEW_HEIGHT, this));
     
@@ -111,25 +125,155 @@ int StudentWorld::move()
     if (randInt(0, ChanceOfLostSoul - 1) == 0)
         actors.push_back(new SoulGoodie(randInt(LEFT_EDGE, RIGHT_EDGE), VIEW_HEIGHT, this));
 
-    /*if (spawnRate(100, 20) == 0)
+    int cur_lane = randInt(1, 3);
+    if (spawnRate(100, 20) == 0)
     {
-        int cur_lane = randInt(1,3);
-        actors.push_back(new ZombieCab(randInt(0, VIEW_WIDTH), VIEW_HEIGHT, this));
-    }*/
+        for (int i = 0; i < 3; i++)
+        {
+            actor* ptr = closestCollisionWorthy(actors, racer, cur_lane);
+            if (ptr == nullptr || (ptr->getY() > VIEW_HEIGHT / 3))
+            {
+                actors.push_back(new ZombieCab(curLaneConversion(cur_lane), SPRITE_HEIGHT / 2, this));
+                vector <actor*> ::reverse_iterator it = actors.rbegin();
+                (*it)->setVSpeed(racer->getVSpeed() + randInt(2, 4));
+                break;
+            }
+            else
+                cur_lane++;
+            if (cur_lane == 4)
+                cur_lane = 1;
+        }
+        
+    }
 
+    bonusPoints--;
+    setGameStatText(text());
     return GWSTATUS_CONTINUE_GAME;
 }
 
 int StudentWorld::spawnRate(int a, int b)
 {
-    int i = max(a - level * 10, b);
+    int L = getLevel();
+    int i = max(a - L * 10, b);
     int j = randInt(0, i - 1);
     return j;
 }
 
 void StudentWorld::recordSoulSaved()
 {
-    numOfSoulsSaved++;
+    numOfSoulsNeeded--;
+}
+
+actor* StudentWorld::closestCollisionWorthy(std::vector <actor*> const& a, actor* racer, int i)
+{
+    int LEFT_EDGE = ROAD_CENTER - ROAD_WIDTH / 2;
+    int LEFT_INNER_EDGE = ROAD_CENTER - ROAD_WIDTH / 6;
+    int RIGHT_EDGE = ROAD_CENTER + ROAD_WIDTH / 2;
+    int RIGHT_INNER_EDGE = ROAD_CENTER + ROAD_WIDTH / 6;
+
+    vector <actor*> ::iterator closest_1;
+    vector <actor*> ::iterator closest_2;
+    vector <actor*> ::iterator closest_3;
+    if (i == 1)
+    {
+        for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); it++)
+        {
+            if ((*it)->getX() >= LEFT_EDGE && (*it)->getX() <= LEFT_INNER_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                closest_1 = it;
+                break;
+            }
+            else
+                closest_1 = actors.end();
+        }
+    }
+    if (i == 2)
+    {
+        for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); it++)
+        {
+            if ((*it)->getX() >= LEFT_INNER_EDGE && (*it)->getX() <= RIGHT_INNER_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                closest_2 = it;
+                break;
+            }
+            else
+                closest_2 = actors.end();
+        }
+    }
+    if (i == 3)
+    {
+        for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); it++)
+        {
+            if ((*it)->getX() >= RIGHT_INNER_EDGE && (*it)->getX() <= RIGHT_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                closest_3 = it;
+                break;
+            }
+            else
+                closest_3 = actors.end();
+        }
+    }
+
+    for (vector <actor*> ::iterator it = actors.begin(); it != actors.end(); it++)
+    {
+        if (i == 1)
+        {
+            if ((*it)->getX() >= LEFT_EDGE && (*it)->getX() <= LEFT_INNER_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                if (closest_1 != actors.end() && (*it)->getY() < (*closest_1)->getY())
+                    closest_1 = it;
+            }
+        }
+        if (i == 2)
+        {
+            if ((*it)->getX() >= LEFT_INNER_EDGE && (*it)->getX() <= RIGHT_INNER_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                if (closest_2 != actors.end() && (*it)->getY() < (*closest_2)->getY())
+                    closest_2 = it;
+            }
+        }
+        if (i == 3)
+        {
+            if ((*it)->getX() >= RIGHT_INNER_EDGE && (*it)->getX() <= RIGHT_EDGE && (*it)->checkCollisionAvoidanceWorthy())
+            {
+                if (closest_3 != actors.end() && (*it)->getY() < (*closest_3)->getY())
+                    closest_3 = it;
+            }
+        }
+    }
+
+
+    if (i == 1 && (racer->getX() >= LEFT_EDGE) && (racer->getX() <= LEFT_INNER_EDGE))
+        return racer;
+    else if (i == 1 && closest_1 != actors.end())
+        return *closest_1;
+    else
+        return nullptr;
+
+    if (i == 2 && (racer->getX() >= LEFT_INNER_EDGE) && (racer->getX() <= RIGHT_INNER_EDGE))
+        return racer;
+    else if (i == 2 && (closest_2 != actors.end()))
+        return *closest_2;
+    else
+        return nullptr;
+
+    if (i == 3 && (racer->getX() >= RIGHT_INNER_EDGE) && (racer->getX() <= RIGHT_EDGE))
+        return racer;
+    else if (i == 3 && closest_3 != actors.end())
+        return *closest_3;
+    else
+        return nullptr;
+   
+}
+
+int StudentWorld::curLaneConversion(int cur_lane)
+{
+    if (cur_lane == 1)
+        return (ROAD_CENTER - ROAD_WIDTH / 3);
+    if (cur_lane == 2)
+        return (ROAD_CENTER);
+    if (cur_lane == 3)
+        return (ROAD_CENTER + ROAD_WIDTH / 3);
 }
 
 bool StudentWorld::sprayFirstAppropriateActor(actor* a)
@@ -139,24 +283,32 @@ bool StudentWorld::sprayFirstAppropriateActor(actor* a)
 
 bool StudentWorld::overlaps(const actor* a1, const actor* a2) const
 {
+
     return true;
+}
+
+void StudentWorld::addSpray(int x, int y, int dir)
+{
+    actors.push_back(new Spray(x, y, dir, this));
 }
 
 void StudentWorld::cleanUp()
 {
     delete racer;
     vector <actor*> ::iterator it;
-    for (it = actors.begin(); it != actors.end(); it++)
+    for (it = actors.begin(); it != actors.end(); )
+    {
         delete* it;
+        it = actors.erase(it);
+    }
 }
 
-/*
 string StudentWorld::text()
 {
+    
     ostringstream top_text;
-    top_text << "Score: " << getScore() << "  Lvl: " << getLevel() << "  Souls2Save: " << "5"
-        << "  Lives: " << getLives() << "  Health: " << "100" << "  Sprays: " << "10" << "  Bonus: " << "5000";
+    top_text << "Score: " << getScore() << "  Lvl: " << getLevel() << "  Souls2Save: " << numOfSoulsNeeded << "  Lives: " << getLives() 
+        << "  Health: " << racer->getHP() << "  Sprays: " << racer->getHolyWaterCharges() << "  Bonus: " << bonusPoints;
     string s = top_text.str();
     return s;
 }
-*/
